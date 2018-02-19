5adf219a1c2542132af432fc3a950ddfba44bc95
Kariel Argenis Díaz Maisonet <kariel.diaz@upr.edu>
2018-02-19 12:49:58 -0400

    Added perimeter feature

--- src/CircleArea.java
+++ src/CircleArea.java
@@ -1,4 +1,15 @@
 
 public class CircleArea {
-
+	public static void main(String[] args) {
+		double radius = 13;
+		System.out.println(area(radius));
+		
+		System.out.println(area(radius)/2);
+	}
+	
+	public static double area(double radius) {
+		double area;
+		area = 2*Math.PI*Math.pow(radius, 2);
+		return area;
+	}
 }
